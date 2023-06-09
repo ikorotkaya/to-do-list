5bf9be3293a9fbf9f1ce86a8686620322abd74b6
ikorotkaya <irinkakorotkaya@gmail.com>
2023-06-09 16:20:21 +0200

    Fetch data from an endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
 import ListHeader from './components/ListHeader';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const userEmail = 'irina@test.com';
+  const [tasks, setTasks] = useState(null);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      setTasks(json)
+    } catch(err) {
+      console.error(err)
+    } 
+  }
+
+  useEffect(() => getData, []);
+  console.log(tasks);
+
   return (
     <div className="app">
       <ListHeader listName={'To do list'} />
--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const PORT = process.env.PORT || 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 //get all todos
-app.get('/todos', async (req, res) => {
-  const userEmail = 'irina@test.com'
+app.get('/todos/:userEmail', async (req, res) => {
+  // if you have one param, you can write this way:
+  // const userEmail = req.params.userEmail;
+  // if you have more than one param, you can write this way:
+  const { userEmail } = req.params;
 
   try {
     const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
